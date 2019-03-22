07f6f8da8153c0ad9a654c3fc4799bfae0544f66
Yeldos Nurymov <39968522+ye1dos@users.noreply.github.com>
2019-03-22 20:15:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ def inside(r, q):
 
 def draw_detections(img, rects, thickness = 1):
     for x, y, w, h in rects:
-        # the HOG detector returns slightly larger rectangles than the real objects.
-        # so we slightly shrink the rectangles to get a nicer output.
         pad_w, pad_h = int(0.15*w), int(0.05*h)
         cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), thickness)
 
