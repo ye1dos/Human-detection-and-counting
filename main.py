53489adfd62c37b62342a179658baa244bafa90f
ye1dos <39968522+ye1dos@users.noreply.github.com>
2019-03-22 18:46:49 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+
+import numpy as np
+import cv2
+from imutils.object_detection import non_max_suppression
+import imutils
+
+count=0
+def inside(r, q):
+    rx, ry, rw, rh = r
+    qx, qy, qw, qh = q
+    return rx > qx and ry > qy and rx + rw < qx + qw and ry + rh < qy + qh
+
+
+def draw_detections(img, rects, thickness = 1):
+    for x, y, w, h in rects:
+        # the HOG detector returns slightly larger rectangles than the real objects.
+        # so we slightly shrink the rectangles to get a nicer output.
+        pad_w, pad_h = int(0.15*w), int(0.05*h)
+        cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), thickness)
+
+
+if __name__ == '__main__':
+
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
+    cap=cv2.VideoCapture(0)
+    while True:
+        _,frame=cap.read()
+        count+=1
+        found,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
+        found_filtered = []
+        for ri, r in enumerate(found):
+            for qi, q in enumerate(found):
+                if ri != qi and inside(r, q):
+                    break
+            else:
+                found_filtered.append(r)
+        draw_detections(frame,found)
+        draw_detections(frame, found_filtered, 3)
+        cv2.putText(frame, str(count),(10,400), cv2.FONT_ITALIC, 2,(255,255,255),2,cv2.LINE_AA)
+        cv2.imshow('frame',frame)
+        print (qi)
+        data = {'Cats':qi}
+        ch = 0xFF & cv2.waitKey(1)
+        if ch == 27:
+            break
+    cv2.destroyAllWindows()
+
+    
